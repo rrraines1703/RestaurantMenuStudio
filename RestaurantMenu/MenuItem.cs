1f3a6cb5c78884d3f471ea8db663848dac2a9b33
Richard Raines <rrraines1703@me.com>
2020-06-05 15:09:54 -0400

    Add project files.

---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class MenuItem
+    {
+        private double price;
+        private List<string> category = new List<string> { "appetizer", "main course", "dessert" };
+        
+        public bool NewItem { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        
+    }
+}
